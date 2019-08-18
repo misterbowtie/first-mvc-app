2356f8dcfc6d40c90987110226d3d0e5860c3cbc
A Tie <misterbowtie@hotmail.com>
2019-08-17 20:23:27 -0400

    added new field for rating

--- MvcMovie/Migrations/20190818002221_InitialCreate.Designer.cs
+++ MvcMovie/Migrations/20190818002221_InitialCreate.Designer.cs
@@ -9,7 +9,7 @@ using MvcMovie.Models;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20190817231446_InitialCreate")]
+    [Migration("20190818002221_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -25,7 +25,10 @@ namespace MvcMovie.Migrations
 
                     b.Property<string>("Genre");
 
-                    b.Property<decimal>("Price");
+                    b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18, 2)");
+
+                    b.Property<string>("Rating");
 
                     b.Property<DateTime>("ReleaseDate");
 
